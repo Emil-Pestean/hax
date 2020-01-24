972ee0effae5f85c176c30aff518fcb66d743845
Emil-Pestean <emil@knoxon.co.uk>
2020-01-24 17:07:23 +0200

    first commit

---
+++ unleash_t3h_bots.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##############################################################
+# This is a script to help with fast install of a new server #
+# things that are installed are:                             #
+# Apache2.4                                                  #
+# Mysql 8                                                    #
+# Redis-server                                               #
+##############################################################
+
+
+# 1. Check for OS type
+if [ -f /etc/os-release ]; then
+	# freedesktop.org and systemd
+	. /etc/os-release
+	OS=$NAME
+	VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+	# linuxbase.org
+	OS=$(lsb_release -si)
+	VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+	# For some versions of Debian/Ubuntu without lsb_release command
+	. /etc/lsb-release
+	OS=$DISTRIB_ID
+	VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+	# Older Debian/Ubuntu/etc.
+	OS=Debian
+	VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+	# Older SuSE/etc.
+	...
+elif [ -f /etc/redhat-release ]; then
+	# Older Red Hat, CentOS, etc.
+	...
+else
+	# Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+	OS=$(uname -s)
+	VER=$(uname -r)
+fi
+
+echo $VERSION_ID
+
+if [ $NAME == "Ubuntu" ]; then
+	echo $NAME
+fi
